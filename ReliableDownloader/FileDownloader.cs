838f76b38daf020b611dcc2f6c80f54e7129c9e5
Oleg Gerasimov <oleg.gerasimov@ifsworld.com>
2021-09-11 12:15:52 +0100

    Partial download working

--- ReliableDownloader/FileDownloader.cs
+++ ReliableDownloader/FileDownloader.cs
@@ -3,12 +3,12 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Collections;
 using System.Linq;
+using System.IO;
 
 namespace ReliableDownloader
 {
     public class FileDownloader : IFileDownloader, IDisposable
     {
-        FileProgress fileProgress;
         CancellationTokenSource cts;
         WebSystemCalls web;
 
@@ -24,6 +24,7 @@ namespace ReliableDownloader
             var bUsePartialDownloader = false;
             Int64? contentLength = -1;
 
+            using var fileStream = new FileStream(localFilePath, FileMode.OpenOrCreate);
             using var headersResult = await web.GetHeadersAsync(contentFileUrl, cancellationToken);
 
             if (headersResult.StatusCode != System.Net.HttpStatusCode.OK)
@@ -50,10 +51,34 @@ namespace ReliableDownloader
                 if (bUsePartialDownloader)
                 {
                     Console.WriteLine("Doing partial download. Content-Length:{0}", contentLength);
+                    var nChunks = 100;  // configurable ?
+                    var chunkSize = (long)contentLength / nChunks;
+                    var remainder = (long)contentLength % nChunks;
+                    var dtStart = DateTime.Now;
+                    var start = 0L;
+                    var end = (long)contentLength;
+
+                    onProgressChanged(new FileProgress(contentLength, 0, null, new TimeSpan(0, 1, 0)));
+                    for (int i = 0; i <= nChunks; ++i)
+                    {
+                        start = chunkSize * i;
+                        if (i<nChunks)
+                            end = chunkSize * (i + 1) - 1;
+                        else
+                            end = start + remainder;
+                        using var getResult = await web.DownloadPartialContent(contentFileUrl, start, end, cancellationToken);
+                        fileStream.Position = start;
+                        await getResult.Content.CopyToAsync(fileStream);
+                        var dtNow = DateTime.Now;
+                        var elapsed = dtNow.Ticks - dtStart.Ticks;
+                        var estimateRemaining = elapsed * ((long)contentLength - end) / (long)contentLength;
+                        onProgressChanged(new FileProgress(contentLength, end, ( (double)100 * end / contentLength), new TimeSpan(estimateRemaining)));
+                    }
+
                 }
                 else
                 {
-                    Console.WriteLine("regular...");
+                    Console.WriteLine("Doing regular download");
                 }
             }
 
