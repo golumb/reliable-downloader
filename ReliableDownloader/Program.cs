94a655fadfb851ccf6900315d5c108aa08cd15c2
Oleg Gerasimov <oleg.gerasimov@ifsworld.com>
2021-09-13 09:33:58 +0100

    partial download working for webclient; recovers after disabling adapter

--- ReliableDownloader/Program.cs
+++ ReliableDownloader/Program.cs
@@ -8,8 +8,10 @@ namespace ReliableDownloader
         public static async Task Main(string[] args)
         {
             // If this url 404's, you can get a live one from https://installerstaging.accurx.com/chain/latest.json.
+
             var exampleUrl = "https://installerstaging.accurx.com/chain/3.94.56148.0/accuRx.Installer.Local.msi";
             var exampleFilePath = "C:/temp/myfirstdownload.msi";
+
             using var fileDownloader = new FileDownloader();
 
 
