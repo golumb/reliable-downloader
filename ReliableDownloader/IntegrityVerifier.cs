a552f250149b3d4b4c8ec62fe60392225d764c15
Oleg Gerasimov <oleg.gerasimov@ifsworld.com>
2021-09-14 22:57:16 +0100

    Added MD5 check; Fixed remaining time estimate

---
+++ ReliableDownloader/IntegrityVerifier.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+
+namespace ReliableDownloader
+{
+    public enum INTEGRITY
+    {
+        NOT_VERIFIED,
+        VERIFIED,
+        COMPROMISED
+    }
+
+    public static class IntegrityVerifier
+    {
+        // Added on 14 Sept 2021
+        // MD5 comparison
+        public static INTEGRITY CheckMD5(byte[] headerHash, string fileName) 
+        {
+            if (headerHash?.Length > 0)
+            {
+                using var fileStream = File.OpenRead(fileName);
+                using var md5 = MD5.Create();
+                var actualHash = md5.ComputeHash(fileStream);
+
+                Console.WriteLine("\n\nHeader hash: {0}", Convert.ToBase64String(headerHash));
+                Console.WriteLine(    "Actual hash: {0}", Convert.ToBase64String(actualHash));
+
+                if (headerHash.SequenceEqual(actualHash))
+                {
+                    return INTEGRITY.VERIFIED;
+                }
+                else
+                {
+                    return INTEGRITY.COMPROMISED;
+                }
+            }
+            else
+            {
+                return INTEGRITY.NOT_VERIFIED;
+            }
+        }
+    }
+}
--- ReliableDownloader/Program.cs
+++ ReliableDownloader/Program.cs
@@ -12,9 +12,13 @@ namespace ReliableDownloader
             var exampleUrl = "https://installerstaging.accurx.com/chain/3.94.56148.0/accuRx.Installer.Local.msi";
             var exampleFilePath = "C:/temp/myfirstdownload.msi";
 
+            //var exampleUrl = "https://sabnzbd.org/tests/internetspeed/20MB.bin";
+            //var exampleFilePath = "C:/temp/20MB.bin";
+
             using var fileDownloader = new FileDownloader();
 
-
+            var stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
 
             Task download = Task.Run(() => fileDownloader.DownloadFileAsync(
                                             exampleUrl,
@@ -24,7 +28,7 @@ namespace ReliableDownloader
                                                     string.Format("{0:N0}%", progress.ProgressPercent),
                                                     string.Format("{0:N1} seconds", progress.EstimatedRemaining?.TotalSeconds));
                                             }));
-            // monitor thread:
+            // monitor task:
             await Task.Run(async () =>
                         {
                             Console.WriteLine("\n\nDownload started. Press 'C' to cancel.\n\n");
@@ -36,9 +40,13 @@ namespace ReliableDownloader
                                   if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.C)
                                       break;
                               }
-                              if (done)
-                                Console.WriteLine("\n\nDownload completed\n\n");                        
-                              else
+                            if (done)
+                            {
+                                stopwatch.Stop();
+                                var _seconds = (decimal)stopwatch.ElapsedMilliseconds / 1000;
+                                Console.WriteLine($"\n\nDownload completed in {_seconds:0.#} seconds\n");
+                            }
+                            else
                                 fileDownloader.CancelDownloads();
                       });
         }
